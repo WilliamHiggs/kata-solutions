caaa2ff97a8920e0a031c2c14460f335253bb42f
William Higgs <williamhiggs@Williams-MacBook-Pro.local>
2018-05-25 23:53:14 +0100

    update

---
+++ xx and yy chromosomes/chromosomeCheck.js
@@ -0,0 +1,10 @@
+
+function chromosomeCheck(x) {
+  var output = "";
+  if(x === "XY") {
+  var output = "Congratulations! You're going to have a son.";
+  } else {
+    var output = "Congratulations! You're going to have a daughter.";
+  }
+  return output;
+}
