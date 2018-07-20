d199ed798ac4bd8d7a8173abe0a0d1dbbb6fe121
William Higgs <32944349+WilliamHiggs@users.noreply.github.com>
2018-07-20 16:16:32 +0100

    update

---
+++ convert string to array/tests.js
@@ -0,0 +1,2 @@
+Test.expect(stringToArray("Robin Singh"), ["Robin", "Singh"]);
+Test.expect(stringToArray("I love arrays they are my favorite"), ["I", "love", "arrays", "they", "are", "my", "favorite"]);
