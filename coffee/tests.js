9bdeab6d72fe8ab9570cf8f16aa5cc3d1a2e7c10
William Higgs <32944349+WilliamHiggs@users.noreply.github.com>
2018-08-01 17:28:24 +0100

    update

---
+++ coffee/tests.js
@@ -0,0 +1,3 @@
+var yourAnswer = coffee("I'm having trouble staying focussed.  Let's get some coffee.");
+var correctAnswer = "I'm having trouble staying focussed.  Let's get some COFFEE.";
+Test.assertEquals(yourAnswer, correctAnswer);
