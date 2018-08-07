2827c7fab8cf338aab40fce48202bd7fe47d4e51
William Higgs <32944349+WilliamHiggs@users.noreply.github.com>
2018-08-07 17:16:56 +0100

    update

---
+++ which is greater/tests.js
@@ -0,0 +1,3 @@
+Test.assertEquals(evenOrOdd('12'), 'Even is greater than Odd');
+Test.assertEquals(evenOrOdd('123'), 'Odd is greater than Even');
+Test.assertEquals(evenOrOdd('112'), 'Even and Odd are the same');
